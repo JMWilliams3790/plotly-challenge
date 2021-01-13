8f139f27a4522809b03205b42455f457d1b0f641
Josh <JMWilliams3790@gmail.com>
2021-01-13 13:16:15 -0800

    Stored data to arrays

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,18 +3,20 @@ var metadata = [];
 var samples = [];
 
 function readData(read) {
-  d3.json("data/samples.json").then((data) => {
+  d3.json("../data/samples.json").then((data) => {
     data.names.forEach((information) => {
       names.push(information);
     });
     data.metadata.forEach((information) => {
       metadata.push(information);
     });
-    data.names.forEach((information) => {
+    data.samples.forEach((information) => {
       samples.push(information);
     });
   })
   .then(read);
 };
-
+function drawData(){
   console.log(names);
+}
+readData(drawData);
