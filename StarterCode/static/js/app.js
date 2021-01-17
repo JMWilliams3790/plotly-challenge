ef9b0b71572791d1f9cb0cd98c3605e7fb18ee9a
Josh <JMWilliams3790@gmail.com>
2021-01-16 18:35:26 -0800

    finished bar chart, and started bubble chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -14,17 +14,75 @@ function readData(read) {
       samples.push(information);
     });
   })
+  
   .then(read);
 };
+
+
+
+//draw first Bar Chart
+function drawChart(){
+  // console.log(names)
+  // console.log(metadata)
+  console.log(samples)
+  var trace1 = {
+    x: samples[0].sample_values.slice(0, 10).reverse(),
+    y: samples[0].otu_ids.slice(0, 10).reverse().map(function (d){
+      return "OTU " + d;
+    }),
+    type: "bar",
+    orientation: "h",
+    text: samples[0].otu_labels.slice(0, 10).reverse()
+  };
+
+  console.log(trace1);
+  // Create the data array for the plot
+  var barData = [trace1];
+
+  // Define the plot layout
+  var layout = {
+    title: "Belly Button Biodiversity Bar Chart"
+  };
+
+  // Plot the chart to a div tag with id "plot"
+  Plotly.newPlot("bar", barData, layout);
+
+
+  var trace2 = {
+    x: samples[0].sample_values,
+    y: samples[0].otu_ids,
+    mode: "markers",
+    marker: {
+      size: samples[0].sample_values
+    },
+  }
+  var bubbleData = [trace2];
+
+  var bubbleLayout = {
+    title: "Belly Button Bideiversity Bubble Chart",
+    showlegend: false,
+  };
+  Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+}
+
+//draw first bubble
+
+
+
+
 //Populate page based on initial state
 function drawData(){
-  console.log(names);
+  
   createDrop(names);
   //Update data with first index in names
   updateMetadata(names[0]);
   //start bar chart initial display
+  drawChart(names);
   //start bubble chart initial display
 }
+
+
+
 readData(drawData);
 
 function createDrop(values){
@@ -42,10 +100,11 @@ function createDrop(values){
 }
 //draw page after changing option in dropdown
 function optionChanged(value){
-  console.log(value);
+ 
   //Update metadata with selected value
   updateMetadata(value);
   //restyle bar chart
+  updateBarChart(value);
   //restyle bubble chart
 }
 
@@ -54,7 +113,7 @@ function updateMetadata(value){
   //filter desired value from metadata
   //update html element with new demo
   var demographic = metadata.filter((demo) => demo.id === parseInt(value));
-  console.log(demographic[0]);
+ 
   //Grabbed element
   var demoPrint = d3.select("#sample-metadata");
   //Cleared html INSIDE element
@@ -63,3 +122,21 @@ function updateMetadata(value){
 }
 
 //plotly.restyle. restyled must be in bracket: [variable]. > html element, attribute, values
+function updateBarChart(value){
+  var sample = samples.filter((d) => d.id === value)[0];
+
+  var X = sample.sample_values.slice(0, 10).reverse();
+
+  Plotly.restyle("bar", "x", [X]);
+  
+  var Y = sample.otu_ids.slice(0, 10).reverse().map(function(d){
+    return "OTU " + d;
+  });
+
+  Plotly.restyle("bar", "y", [Y]);
+
+  var text = sample.otu_labels.slice(0, 10).reverse();
+
+  Plotly.restyle("bar", "text", [text]);
+};
+//restyle. there will be 5
